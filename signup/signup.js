2df8f2ed9cac62f56baf1b1a9e77893fd743ed52
bunsy-0900 <bunsy-chhay@mptc.gov.kh>
2024-07-24 15:28:18 +0700

    feat: adds identity verification status endpoint

--- signup/signup.js
+++ signup/signup.js
@@ -4,7 +4,7 @@ const signupRoute = express.Router();
 
 // const wss = new WS.WebSocketServer({server: WS.Server, path: "/ws"});
 
-const currentTimestamp = () => (new Date()).toISOString();
+const currentTimestamp = () => new Date().toISOString();
 
 const settingsResponse = require("./settings.json");
 const status = require("./status.json");
@@ -13,54 +13,59 @@ const verifyChallenge = require("./verifyChallenge.json");
 const register = require("./register.json");
 const kycProviderList = require("./kycProvidersList.json");
 const termsAndConditions = require("./kyc_terms_condition.json");
-const slot = require("./slot.json")
+const slot = require("./slot.json");
 
 signupRoute.get("/settings", (req, res) => {
-    settingsResponse.responseTime = currentTimestamp();
-    res.send(settingsResponse);
+  settingsResponse.responseTime = currentTimestamp();
+  res.send(settingsResponse);
 });
 
 signupRoute.get("/registration/status", (req, res) => {
-    status.responseTime = currentTimestamp();
-    res.send(status);
+  status.responseTime = currentTimestamp();
+  res.send(status);
 });
 
 signupRoute.get("/registration/register", (req, res) => {
-    register.responseTime = currentTimestamp();
-    res.send(register);
+  register.responseTime = currentTimestamp();
+  res.send(register);
 });
 
 signupRoute.post("/registration/generate-challenge", (req, res) => {
-    challenge.responseTime = currentTimestamp();
-    res.send(challenge);
+  challenge.responseTime = currentTimestamp();
+  res.send(challenge);
 });
 
 signupRoute.post("/registration/verify-challenge", (req, res) => {
-    verifyChallenge.responseTime = currentTimestamp();
-    res.send(verifyChallenge);
+  verifyChallenge.responseTime = currentTimestamp();
+  res.send(verifyChallenge);
 });
 
-
 signupRoute.post("/identity-verification/initiate", (req, res) => {
-    kycProviderList.responseTime = currentTimestamp();
-    res.send(kycProviderList);
-})
+  kycProviderList.responseTime = currentTimestamp();
+  res.send(kycProviderList);
+});
 
 signupRoute.get("/identity-verification/identity-verifier/:id", (req, res) => {
-    res.send({
-        responseTime: currentTimestamp(),
-        errors: null,
-        response: termsAndConditions[req.params.id]
-    });
+  res.send({
+    responseTime: currentTimestamp(),
+    errors: null,
+    response: termsAndConditions[req.params.id],
+  });
 });
 
 signupRoute.post("/identity-verification/slot", (req, res) => {
-    slot.responseTime = currentTimestamp()
-    setTimeout(() => {
-      res.send(slot);
-    }, 2000);
+  slot.responseTime = currentTimestamp();
+  setTimeout(() => {
+    res.send(slot);
+  }, 2000);
 });
 
+signupRoute.get("/identity-verification/status", (req, res) => {
+  status.responseTime = currentTimestamp();
+  setTimeout(() => {
+    res.send(status);
+  }, 2000);
+});
 
 // wss.on("connection", (ws) => {
 //     ws.on('error', (error) => {
